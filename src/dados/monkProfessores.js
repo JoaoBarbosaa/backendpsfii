e917b1a3ac63151b1bd81bf9091c9c2b1546592e
Jaozera <101888305+Jaoozeraa@users.noreply.github.com>
2023-05-20 09:48:45 -0300

    dados professores

---
+++ src/dados/monkProfessores.js
@@ -0,0 +1,16 @@
+var listaProfessores = [
+    {
+        ra: "3457934",
+        sexo: "Masculino",
+        nome: "Marcos João",
+        email: "marcus@gmail.com",
+        turma: "3º Ano",
+        telefone: "22432603",
+        cidade: "Iepê",
+        endereco: "Rua Goiania 27",
+        cep: "1950-000",
+        dataNasc: "19/19/2005"
+    },
+
+]
+export default listaProfessores;
