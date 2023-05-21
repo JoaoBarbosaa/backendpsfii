e90f7144ce41b7dd7216a2a758707c4aa738c392
EmanuelMenossi <manuelmenossi@gmail.com>
2023-05-21 03:08:31 -0300

    Att Cadastro aluno e Autor

--- src/Formularios/FormAutores.jsx
+++ src/Formularios/FormAutores.jsx
@@ -1,77 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import './estilos/FormAutor.css';
+import "./estilos/FormAutor.css";
 
-
-
-export default function FormProfessor(props) {
-
+export default function FormAutor(props) {
   const [validado, setValidado] = useState(false);
-  const [aluno, setAluno] = useState({
-    ra: "",
-    sexo: "",
+  const [autor, setAutor] = useState({
+    cod: "",
     nome: "",
-    email: "",
-    turma: "",
-    telefone: "",
-    cidade: "",
-    endereco: "",
-    cep: "",
-    dataNasc: ""
+    sexo: "",
   });
 
-  function manipularMudanca(e){
+  function manipularMudanca(e) {
     const elemForm = e.currentTarget;
     const id = elemForm.id;
     const valor = elemForm.value;
-    setAluno({...aluno, [id]:valor})
+    setAutor({ ...autor, [id]: valor });
   }
 
-  function manipulaSubmissao (evento) {
+  function manipulaSubmissao(evento) {
     const form = evento.currentTarget;
     if (form.checkValidity()) {
-        let alunos = props.listaAlunos;
-        alunos.push(aluno);
-        props.setAlunos(alunos);
-        props.exibirTabela(true);
-        setValidado(false);
-     }
-    else{
-      setValidado(true)
+      let autores = props.listaAutores;
+      autores.push(autor);
+      props.setAutores(autores);
+      props.exibirTabela(true);
+      setValidado(false);
+    } else {
+      setValidado(true);
     }
     evento.preventDefault();
     evento.stopPropagation();
-    }
-  
+  }
 
-  
   return (
-    
-      <body id="corpo">
-        <Container className="background mb-3">
-          <h1 className='text-center colorWhite'>Cadastro de Autor</h1>
-          <Form noValidate validated={validado} onSubmit={manipulaSubmissao} className='mainForm'>
-            <Form.Group className="mb-3" controlId="RAForm">
-              <Form.Label>Codigo do Autor</Form.Label>
-              <Form.Control type="text" required placeholder="O sistema preenche automaticamente" value={aluno.ra} id="ra" onChange={manipularMudanca} />
-              <Form.Control.Feedback type="invalid">
+    <body id="corpo">
+      <Container className="background mb-3">
+        <h1 className="text-center colorWhite">Cadastro de Autor</h1>
+        <Form
+          noValidate
+          validated={validado}
+          onSubmit={manipulaSubmissao}
+          className="mainForm"
+        >
+          <Form.Group className="mb-3" controlId="CodForm">
+            <Form.Label>Codigo do Autor</Form.Label>
+            <Form.Control
+              type="text"
+              required
+              placeholder="Digite o código do Autor"
+              value={autor.cod}
+              id="cod"
+              onChange={manipularMudanca}
+            />
+            <Form.Control.Feedback type="invalid">
+              Digite um código valido
             </Form.Control.Feedback>
-            </Form.Group>
+          </Form.Group>
 
-
-            <Form.Group className="mb-3" controlId="RAForm">
-              <Form.Label>Nome do Autor</Form.Label>
-              <Form.Control type="text" required placeholder="Digite o nome do Autor" value={aluno.ra} id="ra" onChange={manipularMudanca} />
-              <Form.Control.Feedback type="invalid">
+          <Form.Group className="mb-3" controlId="NomeForm">
+            <Form.Label>Nome do Autor</Form.Label>
+            <Form.Control
+              type="text"
+              required
+              placeholder="Digite o nome do Autor"
+              value={autor.nome}
+              id="nome"
+              onChange={manipularMudanca}
+            />
+            <Form.Control.Feedback type="invalid">
+              Digite um nome valido
             </Form.Control.Feedback>
-            </Form.Group>
+          </Form.Group>
 
-
-            <Form.Group className="mb-3" controlId="SexoForm">
-              <Form.Label>Sexo</Form.Label>
-              <Form.Select value={aluno.sexo} required id="sexo" onChange={manipularMudanca} aria-label="Default select example">
+          <Form.Group className="mb-3" controlId="SexoForm">
+            <Form.Label>Sexo</Form.Label>
+            <Form.Select
+              value={autor.sexo}
+              required
+              id="sexo"
+              onChange={manipularMudanca}
+              aria-label="Default select example"
+            >
               <option value={setValidado}>Selecione uma das opções</option>
               <option value="1">Masculino</option>
               <option value="2">Feminino</option>
@@ -79,23 +90,26 @@ export default function FormProfessor(props) {
             <Form.Control.Feedback type="invalid">
               Selecione seu sexo
             </Form.Control.Feedback>
-            </Form.Group>
+          </Form.Group>
+
+          <div className="botao">
+            <Button variant="secondary"
+              type="button"
+              onClick={() => {
+                props.exibirTabela(true);
+              }}
+            >
+              Voltar
+            </Button>
 
 
+            <Button type="submit" className="botao">
+              Cadastrar
+            </Button>
 
-
-
-
-            
-            <div className="botao">
-            <Button type="submit" className="botao">Cadastrar</Button>
-            
-            <Button type="button" onClick={()=>{
-              props.exibirTabela(true)
-            }}>Voltar</Button>
-            </div>
-          </Form>
-        </Container>
-      </body>
+          </div>
+        </Form>
+      </Container>
+    </body>
   );
 }
--- src/tabelas/TabelaAutores.jsx
+++ src/tabelas/TabelaAutores.jsx
@@ -1,82 +1,105 @@
 import { useState } from "react";
-import { Button, Table, Form, Container, Row, Col} from "react-bootstrap";
+import { Button, Table, Form, Container, Row, Col } from "react-bootstrap";
 
-export default function TabelaAutores(props){
-    const [autores, setAutores] = useState(props.listaAutores);
+export default function TabelaAutores(props) {
+  const [autores, setAutores] = useState(props.listaAutores);
 
-    function excluirAutor(cod){
-        const listaAtualizada = props.listaAutores.filter((autor) => autor.cod !== cod );
-        props.setAutores(listaAtualizada);
-        setAutores(listaAtualizada)
-    }
-
-    function filtrarAlunos(e){
-        const termoBusca = e.currentTarget.value;
-        const resultadoBusca= props.listaAutores.filter((aluno) => aluno.nome.includes(termoBusca))
-        setAutores(resultadoBusca);
-    }
-
-    return (
-        <Container className="border">
-        <h1 className="text-center">Cadastro de Autores</h1>
-        <Button onClick={()=>{
-            props.exibirTabela(false)
-        }}>Cadastrar</Button>
-
-            <Form className="d-flex">
-            <Form.Control type="text" id="termoBusca" onChange={filtrarAlunos} placeholder="Pesquisar"/>
-            <Button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                </svg>
-                </Button>
-            </Form>
-        <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Código</th>
-            <th>Autor</th>
-            <th>Sexo</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-            {   
-                autores?.map((autor)=>{
-                    return <tr key={autor.cod}>
-                        <td>{autor.cod}</td>
-                        <td>{autor.nome}</td>
-                        <td>{autor.sexo}</td>
-                        <td>
-                            <Button><svg xmlns="http://www.w3.org/2000/svg" 
-                                    width="16" height="16" 
-                                    fill="currentColor" 
-                                    className="bi bi-pencil" 
-                                    viewBox="0 0 16 16">
-                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                                    </svg>
-                            </Button>{' '}
-                            <Button onClick={() => {
-                                    if (window.confirm("Deseja realmente excluir o Autor?")){
-                                        excluirAutor(autor.cod);
-                                    }
-                            }}><svg xmlns="http://www.w3.org/2000/svg" 
-                                    width="16" 
-                                    height="16" 
-                                    fill="currentColor" 
-                                    className="bi bi-trash" 
-                                    viewBox="0 0 16 16">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                                    </svg>
-                            </Button>
-                        </td>
-                        </tr>
-                })
-            }
-        </tbody>
-      </Table>
-      </Container>
+  function excluirAutor(cod) {
+    const listaAtualizada = props.listaAutores.filter(
+      (autor) => autor.cod !== cod
     );
+    props.setAutores(listaAtualizada);
+    setAutores(listaAtualizada);
+  }
+
+  function filtrarAlunos(e) {
+    const termoBusca = e.currentTarget.value;
+    const resultadoBusca = props.listaAutores.filter((aluno) =>
+      aluno.nome.includes(termoBusca)
+    );
+    setAutores(resultadoBusca);
+  }
+
+  return (
+    <body id="corpo" className="colorwhite">
+      <Container className="border corpoTabela">
+        <h1 className="text-center">Tabela Cadastro de Autores</h1>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            props.exibirTabela(false);
+          }}
+        >
+          Cadastrar
+        </Button>
+
+        <Form className="d-flex">
+          <Form.Control
+            className="BarraPesquisar"
+            type="text"
+            id="termoBusca"
+            onChange={filtrarAlunos}
+            placeholder="Pesquisar"
+          />
+        </Form>
+        <Table striped bordered hover>
+          <thead className="colorWhite">
+            <tr>
+              <th>Código</th>
+              <th>Autor</th>
+              <th>Sexo</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {autores?.map((autor) => {
+              return (
+                <tr key={autor.cod}>
+                  <td id="colorwhite">{autor.cod}</td>
+                  <td id="colorwhite">{autor.nome}</td>
+                  <td id="colorwhite">{autor.sexo}</td>
+                  <td>
+                    <Button variant="warning">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-pencil"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                      </svg>
+                    </Button>{" "}
+                    <Button
+                      variant="danger"
+                      onClick={() => {
+                        if (
+                          window.confirm("Deseja realmente excluir o Autor?")
+                        ) {
+                          excluirAutor(autor.cod);
+                        }
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-trash"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                      </svg>
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
+    </body>
+  );
 }
-
-
