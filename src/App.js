0ef9d6e8b16a9460892ea3018ec93a752bdeb96a
Jessica <jeessica.lima@outlook.com>
2023-05-29 09:23:25 -0300

    alteracçãp tela cadastro pessoa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import TelaMenu from "./interfaces/TelaMenuSistema.js";
-import FormPessoa from "./interfaces/TelaFormPessoa.js";
+import TelaCadastroPessoa from "./interfaces/TelaCadastroPessoa.jsx";
 import TelaCadastroCategoria from "./interfaces/TelaFormCategoria.js"
 import Tela404 from "./interfaces/Tela404.js"
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TelaCadastroLivro from "./interfaces/TelaCadastroLivro.jsx";
 import TelaCadastroAutor from "./interfaces/TelaFormAutor.js";
 
@@ -11,14 +11,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Routes>
-              <Route path="/cadastroPessoa" element={<FormPessoa></FormPessoa>}></Route>
-              <Route path="/cadastroCategoria" element={<TelaCadastroCategoria></TelaCadastroCategoria>}></Route>
-              <Route path="/cadastroLivro" element={<TelaCadastroLivro/>} />
-              <Route path="/cadastroAutor" element={<TelaCadastroAutor/>} />
-              <Route path="/" element={<TelaMenu></TelaMenu>}></Route>
-              <Route path="*" element={<Tela404></Tela404>}></Route>
-          </Routes>
+        <Routes>
+          <Route path="/cadastroPessoa" element={<TelaCadastroPessoa />} />
+          <Route path="/cadastroCategoria" element={<TelaCadastroCategoria></TelaCadastroCategoria>}></Route>
+          <Route path="/cadastroLivro" element={<TelaCadastroLivro />} />
+          <Route path="/cadastroAutor" element={<TelaCadastroAutor />} />
+          <Route path="/" element={<TelaMenu></TelaMenu>}></Route>
+          <Route path="*" element={<Tela404></Tela404>}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
